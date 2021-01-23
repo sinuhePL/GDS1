27eca2e14400aa2be726b3f328799ed0adeb7ff3
Sooly <gajdzinski.s@gmail.com>
2021-01-23 19:23:40 +0100

    Mine obstacle added.

---
+++ Assets/Scripts/AnimatorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController : MonoBehaviour
+{
+    [SerializeField] string AnimationToPlay;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (!string.IsNullOrEmpty(AnimationToPlay))
+        {
+            animator.Play(AnimationToPlay);
+        }
+    }
+}

bdce281ef448fdc4a4c7d74d1206f9eb1e2dbc0f
Sooly <gajdzinski.s@gmail.com>
2021-01-24 23:29:56 +0100

    Menu sounds added.

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] Image anaglyphImage;
+    [SerializeField] AudioClip mouseOverClip;
+    [SerializeField] AudioClip mouseDownClip;
+
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        anaglyphImage.enabled = false;
+        audioSource.Play();
+    }
+    
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        anaglyphImage.enabled = true;
+        audioSource.Stop();
+    }
+}

5b1982f1538650f68116eb0ec36fc6efbda0fd58
Sooly <gajdzinski.s@gmail.com>
2021-01-27 09:07:15 +0100

    Fixed sorting order of background details on Chapter1 scene.

---
+++ Assets/Scripts/Editor/SetSpriteSortingOrderEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SetSpriteSortingOrder))]
+public class SetSpriteSortingOrderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        SetSpriteSortingOrder myTarget = (SetSpriteSortingOrder)target;
+
+        if (GUILayout.Button("Set sorting order"))
+        {
+            myTarget.SetSortingOrder();
+        }
+    }
+}
---
+++ Assets/Scripts/SetSpriteSortingOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SetSpriteSortingOrder : MonoBehaviour
+{
+    [SerializeField] private int addToCurrentSortingOrder;
+    [SerializeField] private List<Transform> gameObjetParentsToSet;
+
+#if UNITY_EDITOR
+    public void SetSortingOrder()
+    {
+        foreach (Transform parent in gameObjetParentsToSet)
+        {
+            foreach (SpriteRenderer spriteRenderer in parent.GetComponentsInChildren<SpriteRenderer>())
+            {
+                spriteRenderer.sortingOrder += addToCurrentSortingOrder;
+                EditorUtility.SetDirty(spriteRenderer);
+            }
+        }
+
+        gameObjetParentsToSet.Clear();
+    }
+#endif
+}

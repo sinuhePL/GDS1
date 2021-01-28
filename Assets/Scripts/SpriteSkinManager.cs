05b6c575ec48b20dbc4b2be408cb7ec83bff5b60
Sooly <gajdzinski.s@gmail.com>
2021-01-28 14:11:41 +0100

    Fixed animations performance.

---
+++ Assets/Scripts/SpriteSkinManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.U2D.Animation;
+
+public class SpriteSkinManager : MonoBehaviour
+{
+    private SpriteSkin[] spriteSkins;
+
+    private void Awake()
+    {
+        spriteSkins = transform.parent.GetComponentsInChildren<SpriteSkin>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Vehicle")
+        {
+            foreach (SpriteSkin spriteSkin in spriteSkins)
+                spriteSkin.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Vehicle")
+        {
+            foreach (SpriteSkin spriteSkin in spriteSkins)
+                spriteSkin.enabled = false;
+        }
+    }
+}
